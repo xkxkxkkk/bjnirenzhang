b57f5f7305b86125bdfbc0f9c600e4c57cab7159
xkxkxkkk <2308079066@qq.com>
2026-03-23 14:08:12 +0800

    Z_02

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-    server: {
-    host: '0.0.0.0', // 允许外部访问（关键配置）
-    port: 5173, // 端口保持 5173（可自定义）
-    open: false // 可选：是否自动打开浏览器
-  }
 })
